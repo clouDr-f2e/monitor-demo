647d281f9c2f2917160a9a34e489c0a70de03a25
allen <294931812@qq.com>
2021-11-17 14:52:39 +0800

    refactor:add code

--- src/pages/calculator/components/Editor.js
+++ src/pages/calculator/components/Editor.js
@@ -1,26 +1,71 @@
-function Metric({ weightMax, metricScoring }) {
-    return <div>Metric</div>
-}
+import { useMemo } from 'react'
+import { Table } from 'antd'
+import { determineMinMax } from 'src/utils'
 
-function Editor({ metricsData = [] }) {
-    return (
-        <table className='w-full table-fixed'>
-            <thead>
-                <tr className='table-row'>
-                    <th className='table-cell' colSpan='2' />
-                    <th className='table-cell'>Value</th>
-                    <th className='table-cell' />
-                    <th className='table-cell'>Metric Score</th>
-                    <th className='table-cell'>Weighting</th>
-                </tr>
-            </thead>
-            <tbody>
-                {metricsData.map((metric) => {
-                    return <Metric metricScoring={metric.metricScoring} {...metric} />
-                })}
-            </tbody>
-        </table>
-    )
+function Editor({ metricsData = [], onScoreChange, onValueChange }) {
+    const dataSource = useMemo(() => {
+        const weightMax = Math.max(...metricsData.map((m) => m.metricScoring.weight))
+        return metricsData.map((data) => ({ ...data, weightMax }))
+    }, [metricsData])
+
+    const columns = [
+        {
+            title: 'metrics',
+            dataIndex: 'metrics',
+            key: 'metrics',
+            render: (text, record) => <div>{record.name}</div>,
+        },
+        {
+            title: 'Value',
+            dataIndex: 'value',
+            key: 'value',
+            render: (text, { name, metricScoring, value }) => {
+                const { min, max, step } = determineMinMax(metricScoring)
+
+                return (
+                    <>
+                        <input
+                            type='range'
+                            min={min}
+                            value={value}
+                            max={max}
+                            step={step}
+                            className=''
+                            onInput={(e) => onValueChange(name, e.target.valueAsNumber)}
+                        />
+                        <output className='value-output'>{`${value.toLocaleString()}ms`}</output>
+                    </>
+                )
+            },
+        },
+        {
+            title: 'Metric Score',
+            dataIndex: 'score',
+            key: 'score',
+            render: (text, { name, metricScoring, score, weightMax }) => {
+                return (
+                    <>
+                        <input
+                            type='range'
+                            className={` metric-score`}
+                            style={{ width: `${(metricScoring.weight / weightMax) * 100}%` }}
+                            value={score}
+                            onInput={(e) => onScoreChange(name, e.target.valueAsNumber)}
+                        />
+                        <output className={` score-output`}>{score}</output>
+                    </>
+                )
+            },
+        },
+        {
+            title: 'Weighting',
+            dataIndex: 'weight',
+            key: 'weight',
+            render: (text, { metricScoring }) => <span className={` weight-text`}>{metricScoring.weight * 100}%</span>,
+        },
+    ]
+
+    return <Table pagination={false} columns={columns} dataSource={dataSource} />
 }
 
 export default Editor
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { VALUE_AT_QUANTILE } from './math'
+
 const os = (function () {
     const ua = navigator.userAgent
 
@@ -19,4 +21,26 @@ const os = (function () {
     }
 })()
 
-export { os }
+function determineMinMax(metricScoring) {
+    const valueAtScore100 = VALUE_AT_QUANTILE(metricScoring, 0.995)
+    const valueAtScore5 = VALUE_AT_QUANTILE(metricScoring, 0.05)
+
+    let min = Math.floor(valueAtScore100 / 1000) * 1000
+    let max = Math.ceil(valueAtScore5 / 1000) * 1000
+    let step = 10
+
+    // Special handling for CLS
+    if (metricScoring.units === 'unitless') {
+        min = 0
+        max = Math.ceil(valueAtScore5 * 100) / 100
+        step = 0.01
+    }
+
+    return {
+        min,
+        max,
+        step,
+    }
+}
+
+export { os, determineMinMax }
