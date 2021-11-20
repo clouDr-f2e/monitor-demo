d88f56554d3dc783f20bcb056d4c66d0e5a7f9bc
allen <294931812@qq.com>
2021-11-20 20:50:57 +0800

    refactor:edit code

--- src/constants/performance.js
+++ src/constants/performance.js
@@ -2,24 +2,24 @@ const metricsConfig = {
     fcp: {
         '4g': {
             mobile: {
-                weight: 0.3,
+                weight: 0.2,
                 median: 4500,
                 p10: 1500,
             },
             pc: {
-                weight: 0.3,
+                weight: 0.2,
                 median: 4000,
                 p10: 1000,
             },
         },
         '3g': {
             mobile: {
-                weight: 0.3,
+                weight: 0.2,
                 median: 6000,
                 p10: 3000,
             },
             pc: {
-                weight: 0.3,
+                weight: 0.2,
                 median: 6000,
                 p10: 2500,
             },
@@ -54,24 +54,24 @@ const metricsConfig = {
     ccp: {
         '4g': {
             mobile: {
-                weight: 0.4,
+                weight: 0.5,
                 median: 7500,
                 p10: 3500,
             },
             pc: {
-                weight: 0.4,
+                weight: 0.5,
                 median: 7000,
                 p10: 3000,
             },
         },
         '3g': {
             mobile: {
-                weight: 0.4,
+                weight: 0.5,
                 median: 9000,
                 p10: 5000,
             },
             pc: {
-                weight: 0.4,
+                weight: 0.5,
                 median: 8500,
                 p10: 4500,
             },
